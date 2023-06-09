d57ba589694f8979dcc9cb3bac517943c64c088d
David-Vandensteen <dvandensteen@gmail.com>
2023-06-09 16:02:55 +0200

    Update : emit with name space ref on store lib

--- src/lib/store.js
+++ src/lib/store.js
@@ -3,16 +3,16 @@ import EventEmitter from 'eventemitter3';
 const defaultStore = 'defaultStore';
 
 export default class Store extends EventEmitter {
-  get(key, { nameSpace } = { nameSpace: defaultStore }) {
+  get(key, { nameSpace = defaultStore } = {}) {
     if (!this[nameSpace]) return undefined;
     return this[nameSpace][key];
   }
 
-  set(key, value, { nameSpace } = { nameSpace: defaultStore }) {
+  set(key, value, { nameSpace = defaultStore } = {}) {
     if (!this[nameSpace]) this[nameSpace] = {};
     if (this[nameSpace][key] !== value) {
       this[nameSpace][key] = value;
-      this.emit(key, value); // TODO : nameSpace
+      this.emit(`${nameSpace}:${key}`, value);
     }
   }
 }
